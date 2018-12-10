edae4fc42ec32dc8c680d2ee7f3b67f6040069c1
bendavidelad <elad.bendavid1@mail.huji.ac.il>
2018-12-10 20:43:29 +0200

    pulling added

---
+++ WhistleBlowerServer/src/main/java/com/whistleBlower/business_logic/BusinessLogic.java
@@ -0,0 +1,73 @@
+package com.whistleBlower.business_logic;
+
+import com.whistleBlower.model.Group;
+import com.whistleBlower.model.Message;
+import com.whistleBlower.model.User;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+
+public class BusinessLogic {
+
+    private List<Message> messages;
+    private List<Group> groups;
+    private List<User> users;
+
+    public BusinessLogic() {
+        messages = new ArrayList<>();
+        groups = new ArrayList<>();
+        users = new ArrayList<>();
+    }
+
+
+    public void createUser(User user){
+        users.add(user);
+    }
+
+    public void createGroup(Group group){
+        groups.add(group);
+        users.forEach(user -> addGroupToUser(group, user));
+    }
+
+    public void sendMessage(Message message){
+        messages.add(message);
+        Group group = message.getGroup();
+        List<User> users = group.getUsers();
+        users.forEach(user -> sendMessageToUser(user, message));
+    }
+
+    private void addGroupToUser(Group group, User user){
+        user.addGroup(group);
+    }
+
+    public List<Group> pullGroupsForUser(String phoneNumber){
+        Optional<User> user = users.stream().filter(curr_user -> curr_user.getPhoneNumber().equals(phoneNumber)).findAny();
+        if (user.isPresent()){
+            return user.get().pullGroups();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private void sendMessageToUser(User user, Message message){
+        user.addMessage(message);
+    }
+
+    public List<Message> pullMessagesForUser(String phoneNumber){
+        Optional<User> user = users.stream().filter(curr_user -> curr_user.getPhoneNumber().equals(phoneNumber)).findAny();
+        if (user.isPresent()){
+            return user.get().pullMessages();
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+
+    }
---
+++ WhistleBlowerServer/src/main/java/com/whistleBlower/model/Message.java
@@ -0,0 +1,22 @@
+package com.whistleBlower.model;
+
+public class Message {
+
+    private String content;
+    private User sender;
+    private Group group;
+    private long time;
+
+    Message(String content, User sender, Group group) {
+        this.content = content;
+        this.sender = sender;
+        this.group = group;
+        this.time = System.currentTimeMillis();
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+
+}
