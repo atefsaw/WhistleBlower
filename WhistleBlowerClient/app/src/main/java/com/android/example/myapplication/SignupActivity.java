d6c0659687d9dd85cc44c8543abb671f75d35832
atefsaw <atefwsawaed@gmail.com>
2019-01-13 20:02:43 +0200

    Added user Signup local database to keep him signed in

--- WhistleBlowerClient/app/src/main/java/com/android/example/myapplication/SignupActivity.java
+++ WhistleBlowerClient/app/src/main/java/com/android/example/myapplication/SignupActivity.java
@@ -1,6 +1,8 @@
 package com.android.example.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,7 +23,24 @@ public class SignupActivity extends AppCompatActivity {
                              WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
-        setContentView(R.layout.introduction_signup);
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.USER_SIGNUP_SP_FILE), Activity.MODE_PRIVATE);
+        boolean alreadySignedUp = sharedPreferences.getBoolean(getString(R.string.USER_SIGNUP_SP_KEY), false);
+        String userPhoneNumber = sharedPreferences.getString(getString(R.string.USER_SIGNUP_SP_PHONENUMBER), "");
+
+        if (alreadySignedUp) {
+            Intent groupsActivityIntent = new Intent(this, GroupsActivity.class);
+            groupsActivityIntent.putExtra(getString(R.string.phoneNumberIntentKey), userPhoneNumber);
+            groupsActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(groupsActivityIntent);
+            finish(); // close the signup
+        }
+        else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(getString(R.string.USER_SIGNUP_SP_KEY), false); // Save this locally to not prompt register layout again
+            editor.apply();
+            setContentView(R.layout.introduction_signup); // TODO: check if this needs to be before startActivity()
+
+        }
     }
 
     /**
@@ -40,10 +59,18 @@ public class SignupActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.USER_SIGNUP_SP_FILE), Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(getString(R.string.USER_SIGNUP_SP_KEY), true); // Save this locally to not prompt register layout again
+        editor.putString(getString(R.string.USER_SIGNUP_SP_PHONENUMBER), userPhoneNumber);
+        editor.apply();
+
+
         Intent groupsActivityIntent = new Intent(this, GroupsActivity.class);
         groupsActivityIntent.putExtra(getString(R.string.phoneNumberIntentKey), userPhoneNumber);
         if (!userPhoneNumber.equals("")) {
             startActivity(groupsActivityIntent);
+            finish();
         }
     }
 
